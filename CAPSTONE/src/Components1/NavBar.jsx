aa040f1552afa3ffbc050f02a79733eefe053bc6
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2024-03-15 16:32:59 +0200

    final

--- CAPSTONE/src/Components1/NavBar.jsx
+++ CAPSTONE/src/Components1/NavBar.jsx
@@ -1,20 +1,14 @@
-export default function NavBar(){
-return(
-
-<div>
-
-    
-<nav className="title">
-    HereToHear
-</nav>
-
-
-</div>
-
-
-
-)
-
-
-
+export default function NavBar() {
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="title">
+          HereToHear
+        </div>
+        <div className="search-button">
+          <button>Search</button>
+        </div>
+      </nav>
+    </div>
+  );
 }
