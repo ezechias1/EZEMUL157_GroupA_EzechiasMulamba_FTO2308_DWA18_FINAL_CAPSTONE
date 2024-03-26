b3b507a59cf4b6ba0ad9a4ded789da0fed028b92
Ezechias Mulamba <ezechiasmulamba@gamil.com>
2024-03-26 16:12:32 +0200

    final

--- CAPSTONE/src/Components1/NavBar.jsx
+++ CAPSTONE/src/Components1/NavBar.jsx
@@ -1,6 +1,6 @@
 
 // eslint-disable-next-line react/prop-types
-export default function NavBar({onButtonClick, onButtonClick1 , onButtonClick2}) {
+export default function NavBar({onButtonClick, onButtonClick1 , onButtonClick2, onButtonClick3}) {
   return (
     <div>
       <nav className="navbar">
@@ -12,7 +12,8 @@ export default function NavBar({onButtonClick, onButtonClick1 , onButtonClick2})
        <button className="FilterButton" onClick={onButtonClick1}>
           from z-a filter
        </button> <br/>
-       <button className="FilterButton" onClick={onButtonClick2}>date filter</button>
+       <button className="FilterButton" onClick={onButtonClick2}>date filter</button><br/>
+       <button className="FilterButton" onClick={onButtonClick3}>Favourites</button>
        
 
       </div>
